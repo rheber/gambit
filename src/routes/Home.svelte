34534495d28e1da53dd60f0b0765dd1387ce520b
rheber <rheber@rediffmail.com>
2022-10-21 21:32:35 +1100

    display chessboard

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,1 +1,17 @@
+<script>
+    import { Chessboard } from "@discape/chessboardjs";
+
+    export const params = {};
+
+    const loadBoard = () => {
+	var board1 = Chessboard('board1', 'start');
+    };
+</script>
+
 <h1>Gambit</h1>
+
+<div use:loadBoard id="board1" style="width: 400px"></div>
+
+<style lang="scss" global>
+@import "../../node_modules/@discape/chessboardjs/dist/build/chessboard";
+</style>
