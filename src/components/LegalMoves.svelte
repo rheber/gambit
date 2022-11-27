178ed172dc72aeef8003fc7114cfa5aded3bb907
rheber <rheber@rediffmail.com>
2022-11-27 14:52:07 +1100

    put moves into table

--- src/components/LegalMoves.svelte
+++ src/components/LegalMoves.svelte
@@ -4,17 +4,34 @@
 
 <div class="root">
   <em>Legal Moves ({moves.length} total)</em>
-  {#each moves as moveObject}
-    <div class="move">
-      <div>{moveObject.move}</div>
-	{#await moveObject.lichessData then lichessData}
-	  <div>{lichessData.opening.name}</div>
-	  <div>{lichessData.white}</div>
-	  <div>{lichessData.draws}</div>
-	  <div>{lichessData.black}</div>
-        {/await}
-    </div>
-  {/each}
+  <table>
+    <tr>
+      <th>Move</th>
+      <th>Name</th>
+      <th>Games</th>
+      <th>White</th>
+      <th>Draw</th>
+      <th>Black</th>
+    </tr>
+    {#each moves as moveObject}
+      <tr class="move">
+	  <td class="right">{moveObject.move}</td>
+	  {#await moveObject.lichessData then lichessData}
+	    <th class="left">{lichessData.opening?.name || ""}</th>
+	    <td class="right">
+	      {
+		(lichessData.white || 0) +
+		(lichessData.draws || 0) +
+		(lichessData.black || 0)
+	      }
+	    </td>
+	    <td class="right">{lichessData.white}</td>
+	    <td class="right">{lichessData.draws}</td>
+	    <td class="right">{lichessData.black}</td>
+	  {/await}
+      </tr>
+    {/each}
+  </table>
 </div>
 
 <style>
@@ -23,8 +40,15 @@
     width: 100%;
   }
 
-  .move {
-    display: flex;
-    justify-content: space-between;
+  .left {
+    text-align: left;
+  }
+
+  .right {
+    text-align: right;
+  }
+
+  tr:nth-child(even) {
+    background-color: #f2f2f2;
   }
 </style>
